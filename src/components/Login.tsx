41b2020165c59f8aa1edd8040bef807cc0a7e1ca
HazAnwar <3630449+HazAnwar@users.noreply.github.com>
2020-04-18 23:33:13 +0500

    initial commit

---
+++ src/components/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Login = () => {
+  return (
+    <div className="login">
+      <form>
+        <input type="email">Please enter email address</input>
+        <input type="password">Please enter email address</input>
+      </form>
+    </div>
+  );
+};
